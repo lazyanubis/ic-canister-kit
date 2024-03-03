fbf8da7d4d23c47c6b414d8f4fffff5ced8ecf3e
anubis <anubis@users.noreply.github.com>
2024-03-04 00:46:24 +0800

    canister cycles

--- src/canister/cycles.rs
+++ src/canister/cycles.rs
@@ -1,8 +1,4 @@
-use ic_cdk::api::management_canister::provisional::CanisterIdRecord;
-
 use crate::identity::CanisterId;
-
-use super::{types::CallError, unwrap_call_result};
 
 /// 和 罐子 的 Cycles 相关
 /*
@@ -15,35 +11,29 @@ pub fn wallet_balance() -> candid::Nat {
 }
 
 #[ic_cdk::update]
-pub fn wallet_receive() -> ic_canister_kit::canister::cycles::WalletReceiveResult {
+pub fn wallet_receive() -> candid::Nat {
     ic_canister_kit::canister::cycles::wallet_receive()
 }
 
 */
 
-/// 接受转入 cycles 的结果类型
-#[derive(candid::CandidType, candid::Deserialize, Debug)]
-pub struct WalletReceiveResult {
-    pub accepted: u64,
-}
-
 /// 通用的查询罐子剩余 cycles 的接口
 #[inline]
 pub fn wallet_balance() -> candid::Nat {
-    return candid::Nat::from(ic_cdk::api::canister_balance128()); // Cycles.balance()
+    candid::Nat::from(super::self_canister_cycles()) // Cycles.balance()
 }
 
 /// 接受转入 cycles
-// #[inline]
-pub fn wallet_receive<F>(callback: F) -> WalletReceiveResult
+#[inline]
+pub fn wallet_receive<F>(callback: F) -> candid::Nat
 where
-    F: Fn(u128),
+    F: FnOnce(u128),
 {
     // 获取调用者转入的可接受的 cycles 数量
     let available = ic_cdk::api::call::msg_cycles_available128(); // Cycles.available();
 
     if available == 0 {
-        return WalletReceiveResult { accepted: 0 };
+        return candid::Nat::from(0_u128);
     }
 
     // 接受所有的转入
@@ -55,43 +45,70 @@ where
     callback(accepted); // 回调
 
     // 返回接受的 cycles 数量
-    WalletReceiveResult {
-        accepted: accepted as u64,
-    }
+    candid::Nat::from(accepted)
 }
 
 // 充值余额
-// ! 是 Controller 才能充值
-pub async fn deposit_cycles(canister_id: CanisterId, cycles: u128) -> Result<(), String> {
+// ! 必须是 Controller 才能充值
+pub async fn deposit_cycles(
+    canister_id: CanisterId,
+    cycles: u128,
+) -> Result<(), super::types::CanisterCallError> {
     let call_result = ic_cdk::api::management_canister::main::deposit_cycles(
-        CanisterIdRecord { canister_id },
+        ic_cdk::api::management_canister::provisional::CanisterIdRecord { canister_id },
         cycles,
     )
     .await;
-    if call_result.is_err() {
-        let err = call_result.unwrap_err();
-        return Result::Err(format!(
-            "canister: {} deposit_cycles {} failed: {:?} {}",
-            canister_id.to_text(),
-            cycles,
-            err.0,
-            err.1
-        ));
-    }
-    Result::Ok(())
+    call_result.map_err(
+        |(rejection_code, message)| super::types::CanisterCallError {
+            canister_id,
+            method: "deposit_cycles".into(),
+            rejection_code,
+            message,
+        },
+    )
 }
 
 // 查询罐子余额
-pub async fn call_wallet_balance(canister_id: &CanisterId) -> candid::Nat {
-    let call_result: Result<(candid::Nat,), CallError> =
-        ic_cdk::api::call::call(canister_id.clone(), "wallet_balance", ()).await;
-    unwrap_call_result(canister_id, "wallet_balance", call_result)
+// ! 必须实现 wallet_balance : () -> (nat) query 接口
+pub async fn call_wallet_balance(
+    canister_id: CanisterId,
+) -> super::types::CanisterCallResult<candid::Nat> {
+    let call_result =
+        ic_cdk::api::call::call::<(), (candid::Nat,)>(canister_id, "wallet_balance", ()).await;
+    call_result
+        .map(|(balance,)| balance)
+        .map_err(
+            |(rejection_code, message)| super::types::CanisterCallError {
+                canister_id,
+                method: "deposit_cycles".into(),
+                rejection_code,
+                message,
+            },
+        )
 }
 
 // 充值罐子余额
-pub async fn call_wallet_receive(canister_id: &CanisterId, cycles: u64) -> WalletReceiveResult {
-    let call_result: Result<(WalletReceiveResult,), CallError> =
-        ic_cdk::api::call::call_with_payment(canister_id.clone(), "wallet_receive", (), cycles)
-            .await;
-    unwrap_call_result(canister_id, "wallet_receive", call_result)
+// ! 必须实现 wallet_receive : () -> (nat) 接口
+pub async fn call_wallet_receive(
+    canister_id: CanisterId,
+    cycles: u64,
+) -> super::types::CanisterCallResult<candid::Nat> {
+    let call_result = ic_cdk::api::call::call_with_payment::<(), (candid::Nat,)>(
+        canister_id,
+        "wallet_receive",
+        (),
+        cycles,
+    )
+    .await;
+    call_result
+        .map(|(balance,)| balance)
+        .map_err(
+            |(rejection_code, message)| super::types::CanisterCallError {
+                canister_id,
+                method: "wallet_receive".into(),
+                rejection_code,
+                message,
+            },
+        )
 }
--- src/canister/types.rs
+++ src/canister/types.rs
@@ -1,17 +1,38 @@
-#[cfg(feature = "canister_cycles")]
-pub use super::cycles::WalletReceiveResult;
-
-#[cfg(feature = "canister_status")]
-pub use super::status::{CanisterInfoResult, CanisterStatusResult};
-
-#[cfg(feature = "canister_call")]
-pub use super::deploy::{CanisterInfo, CanisterInfoShow};
-
-#[cfg(feature = "canister_managed")]
-pub use super::managed::{
-    ManagedCanisterChecking, ManagedCanisterConfig, ManagedCanisterInitial,
-    ManagedCanisterMaintained, ManagedCanisterRefreshResult, ManagedCanisterStates,
-};
-
+// ================== 罐子调用产生的错误信息 ==================
 // 罐子调用会产生的错误
-pub type CallError = (ic_cdk::api::call::RejectionCode, std::string::String);
+#[derive(Debug)]
+pub struct CanisterCallError {
+    pub canister_id: crate::identity::CanisterId,
+    pub method: String,
+    pub rejection_code: ic_cdk::api::call::RejectionCode,
+    pub message: std::string::String,
+}
+impl std::fmt::Display for CanisterCallError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Canister({}) call({}) failed. {:?} ({})",
+            self.canister_id.to_text(),
+            self.method,
+            self.rejection_code,
+            self.message
+        )
+    }
+}
+impl std::error::Error for CanisterCallError {}
+
+pub type CanisterCallResult<T> = Result<T, CanisterCallError>;
+
+// ===================== 常用模块 =====================
+
+// #[cfg(feature = "canister_status")]
+// pub use super::status::{CanisterInfoResult, CanisterStatusResult};
+
+// #[cfg(feature = "canister_call")]
+// pub use super::deploy::{CanisterInfo, CanisterInfoShow};
+
+// #[cfg(feature = "canister_managed")]
+// pub use super::managed::{
+//     ManagedCanisterChecking, ManagedCanisterConfig, ManagedCanisterInitial,
+//     ManagedCanisterMaintained, ManagedCanisterRefreshResult, ManagedCanisterStates,
+// };
