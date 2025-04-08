76ba057b935d3f1e6c6059828b7fecc789497695
anubis <anubis@users.noreply.github.com>
2025-04-08 22:00:26 +0800

    functions: upgrade

--- src/functions/upgrade.rs
+++ src/functions/upgrade.rs
@@ -4,12 +4,15 @@
 
 /// 版本升级
 pub trait Upgrade<T> {
+    /// 进行升级
+    fn upgrade(&mut self, arg: T);
+}
+
+/// 状态升级
+pub trait StateUpgrade<T>: Upgrade<T> {
     /// 当前版本
     fn version(&self) -> u32;
 
     /// 获取指定版本的默认数据
     fn from_version(version: u32) -> Self;
-
-    /// 进行升级
-    fn upgrade(&mut self, arg: T);
 }
